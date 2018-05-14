8d90ec3b53ea6c325be2ef3ad63ca4a7d0ecf033
jimliuxinghai <jimliuxinghai@163.com>
2018-05-14 13:52:47 +0800

    'login'

--- server/api/login.js
+++ server/api/login.js
@@ -17,7 +17,7 @@ export async function login (ctx, config = {}) {
 		let ok = null
 		if(reqData.data.retCode == 0) {
 			ctx.session.isLogin = true
-			console.log(reqData.data.data.id)
+			ctx.session.phone = reqData.data.data.phone
 			ctx.session.id = reqData.data.data.id
 			ok = util.errorModal('ERR_OK')
 		}
