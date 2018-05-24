46684b74fd5ceb3ed7d8a922c263a1ac22c324d2
jimliuxinghai <jimliuxinghai@163.com>
2018-05-24 09:14:26 +0800

    chart

--- src/app/analysis/index.vue
+++ src/app/analysis/index.vue
@@ -4,12 +4,12 @@
 <template>
     <div class="checker">
         <div class="top" style="padding-left: 30px;">
-            <!--  <Input v-model="username" placeholder="用户名" style="width: 300px"></Input>
-             <Button type="primary" @click="getData">查询</Button>
-             <Button type="info" @click="modal = true">添加验票员</Button> -->
+            <span>月份:</span>
+            <DatePicker  v-model="date"  @on-change="changeMon" type="month" placeholder="选择月份" style="width: 200px"></DatePicker>
         </div>
         <div class="inner">
             <highcharts :options="chartOptions" ref="highcharts"></highcharts>
+            <highcharts :options="cityOptions" ref="highcharts"></highcharts>
         </div>
     </div>
 </template>
@@ -20,7 +20,9 @@
 
     export default {
         beforeMount () {
+            this.date = this.getDate(new Date(), 'yyyy-MM')
             this.getData();
+
         },
         data () {
             return {
@@ -29,14 +31,23 @@
                 tableData: [],
                 total: 0,
                 page: 0,
+                date: '',
                 hourData: [],
                 cityData: [],
-                chartOptions: {}
-
+                chartOptions: {},
+                cityOptions: {},
+                param: {
+                    year: '',
+                    month: '',
+                    productCode: null
+                }
             }
         },
         methods: {
             async getData () {
+                let newDate = this.date.split('-')
+                this.param.year = newDate[0]
+                this.param.month = newDate[1]
                 let param = {
                     "year":"2018",
                     "month":"4",
@@ -56,6 +67,13 @@
                 // this.total = data.totalElements
                 
             },
+            changeMon(date) {
+                
+                let newDate = date.split('-')
+                this.param.year = newDate[0]
+                this.param.month = newDate[1]
+                
+            },
             dealData(hourData, cityData) {
                 let hourArr = []
                 for(let i = 1; i <= 24; i++ ) {
@@ -73,11 +91,50 @@
                     })
                     hourArr.push(newData)
                 }
-
+                this.init_city(cityData)
                 this.init_charts(hourArr)
             },
-            init_charts(data, callback){
+            init_city(data) {
+                  let xData = []
+                  let yData = []
+
+                  data.forEach((item) => {
+                      if(item.cityName) {
+                        xData.push(item.cityName)
+                        yData.push(item.count)
+                      }
+                      
+                  })
+                this.cityOptions = {
+                  chart: {
+                      type: 'column'
+                  },
+                  title: {
+                      text: '销售量城市分布'
+                  },
                 
+                  xAxis: {
+                      categories: xData,
+                      crosshair: true
+                  },
+                  yAxis: {
+                      min: 0,
+                      title: {
+                          text: '售票数'
+                      }
+                  },
+                  plotOptions: {
+                      column: {
+                          borderWidth: 0
+                      }
+                  },
+                  series: [{
+                      name: '城市销售量',
+                      data: yData
+                  }]
+                };
+            },
+            init_charts(data, callback){
                 let xData = []
                 let yData = []
 
@@ -114,6 +171,31 @@
                 }]
               };
 
+            },
+            getDate(d, format) {
+                const o = {
+                    'M+': d.getMonth() + 1,
+                    'd+': d.getDate(),
+                    'h+': d.getHours(),
+                    'm+': d.getMinutes(),
+                    's+': d.getSeconds(),
+                    'q+': Math.floor((d.getMonth() + 3) / 3),
+                    'S': d.getMilliseconds()
+                };
+
+                if (/(y+)/.test(format)) {
+                    format = format.replace(RegExp.$1, (d.getFullYear() + '').substr(4
+                        - RegExp.$1.length));
+                }
+
+                for (let k in o) {
+                    if (new RegExp('(' + k + ')').test(format)) {
+                        format = format.replace(RegExp.$1, RegExp.$1.length == 1
+                            ? o[k]
+                            : ('00' + o[k]).substr(('' + o[k]).length));
+                    }
+                }
+                return format;
             }
         },
         components: {
